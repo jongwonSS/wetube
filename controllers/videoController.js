746f364c0aa9f523dbd685bef60478a648999f29
JongWon Song <foemzos@gmail.com>
2020-01-19 23:12:24 +0900

    2020/01/19_2

---
+++ controllers/videoController.js
@@ -0,0 +1,2 @@
+export const home = (req, res) => res.send("Videos");
+export const search = (req, res) => res.send("search");
--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send("uesr index"));
-userRouter.get("/edit", (req, res) => res.send("uesr edit"));
-userRouter.get("/password", (req, res) => res.send("uesr password"));
